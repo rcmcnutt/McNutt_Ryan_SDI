62de6548c46da6c99465dce27e9246ffe1af0894
rcmcnutt <ryanmcnutt@Ryans-MacBook-Pro.local>
2015-07-09 05:56:43 -0400

    More Updates
    Added more variables and comments to the .js file.

--- McNutt_Ryan_Output_Assignment/js/McNutt_Ryan_Output_Assignments.js
+++ McNutt_Ryan_Output_Assignment/js/McNutt_Ryan_Output_Assignments.js
@@ -2,11 +2,13 @@
 //0004659907
 //Scalable Data Infrastructures
 //C201507 Term
-//Getting To Know You
+//Getting started with Social Media
 
-var twitter = "rcmcnutt09"; //will output twitter account in output
-var numFollowers = "2"; //will output number of twitter followers
-var whoIFollow = "16"; //will output number of people I follow on twitter
+var isMedia = true; //Boolean variable, that shows I have a twitter account
+var twitter = "rcmcnutt09"; //String variable, will output twitter account in output
+var numFollowers = 2; //Number variable, will output number of twitter followers
+var whoIFollow = 16; //Number variable, will output number of people I follow on twitter
 
-document.write ("My Twitter handle is " + twitter + "." ); //output information on index.html
-document.write ("Currently I have " + numFollowers + " followers and I'm currently following " + whoIFollow + "."); //output more information on index.html
+document.write ("It's " + isMedia + ", I have a Twitter account.");//output information on index.html
+document.write (" My Twitter handle is " + twitter + "." ); //output information on index.html
+document.write (" Currently I have " + numFollowers + " followers, and I'm currently following " + whoIFollow + "."); //output more information on index.html
