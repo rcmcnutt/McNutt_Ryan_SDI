e244d95c87d99a1cf16da45ebbbed1af59489593
rcmcnutt <ryanmcnutt@Ryans-MacBook-Pro.local>
2015-07-09 06:18:35 -0400

    Formatting
    Added line breaks to the document.write so the display on the
    index.html file flows better. Checking output on a few web browsers to
    make sure they show the same information.

--- McNutt_Ryan_Output_Assignment/js/McNutt_Ryan_Output_Assignments.js
+++ McNutt_Ryan_Output_Assignment/js/McNutt_Ryan_Output_Assignments.js
@@ -9,6 +9,6 @@ var twitter = "rcmcnutt09"; //String variable, will output twitter account in ou
 var numFollowers = 2; //Number variable, will output number of twitter followers
 var whoIFollow = 16; //Number variable, will output number of people I follow on twitter
 
-document.write ("It's " + isMedia + ", I have a Twitter account.");//output information on index.html
-document.write (" My Twitter handle is " + twitter + "." ); //output information on index.html
-document.write (" Currently I have " + numFollowers + " followers, and I'm currently following " + whoIFollow + "."); //output more information on index.html
+document.write ("It's " + isMedia + ", I have a Twitter account." + "<br \>" ); //output information on index.html
+document.write (" My Twitter handle is " + twitter + "." + "<br \>"); //output information on index.html
+document.write (" Currently I have " + numFollowers + " followers, and I'm currently following " + whoIFollow + " users."); //output more information on index.html
