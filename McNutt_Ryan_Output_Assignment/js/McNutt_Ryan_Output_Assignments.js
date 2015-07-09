2fd329c0224b3ce83ebd8922b5702031e1eacbcf
rcmcnutt <ryanmcnutt@Ryans-MacBook-Pro.local>
2015-07-09 06:50:11 -0400

    Var fix
    Updated the name of Boolean variable to conform with assignment rubric.

--- McNutt_Ryan_Output_Assignment/js/McNutt_Ryan_Output_Assignments.js
+++ McNutt_Ryan_Output_Assignment/js/McNutt_Ryan_Output_Assignments.js
@@ -4,11 +4,14 @@
 //C201507 Term
 //Getting started with Social Media
 
-var isMedia = true; //Boolean variable, that shows I have a twitter account
+var hasMedia = true; //Boolean variable, that shows I have a twitter account
 var twitter = "rcmcnutt09"; //String variable, will output twitter account in output
 var numFollowers = 2; //Number variable, will output number of twitter followers
 var whoIFollow = 16; //Number variable, will output number of people I follow on twitter
 
-document.write ("It's " + isMedia + ", I have a Twitter account." + "<br \>" ); //output information on index.html
+alert("Hello Professor Story!"); //displays a greeting
+alert("What follows is information on my Twitter account."); //another alert about information that will be displayed
+
+document.write ("It's " + hasMedia + ", I have a Twitter account." + "<br \>" ); //output information on index.html
 document.write (" My Twitter handle is " + twitter + "." + "<br \>"); //output information on index.html
 document.write (" Currently I have " + numFollowers + " followers, and I'm currently following " + whoIFollow + " users."); //output more information on index.html
