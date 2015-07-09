88eaeca47396ae0c9c7f6886fb5c0af8c177ed9f
rcmcnutt <ryanmcnutt@Ryans-MacBook-Pro.local>
2015-07-09 05:24:24 -0400

    Variables
    Created multiple variables and setup output to the html file index.html.

--- McNutt_Ryan_Output_Assignment/js/McNutt_Ryan_Output_Assignments.js
+++ McNutt_Ryan_Output_Assignment/js/McNutt_Ryan_Output_Assignments.js
@@ -1,6 +1,12 @@
 //Ryan McNutt
 //0004659907
-//Scalable Data Infrastuctures
+//Scalable Data Infrastructures
 //C201507 Term
 //Getting To Know You
 
+var twitter = "rcmcnutt09"; //will output twitter account in output
+var numFollowers = "2"; //will output number of twitter followers
+var whoIFollow = "16"; //will output number of people I follow on twitter
+
+document.write ("My Twitter handle is " + twitter + "." ); //output information on index.html
+document.write ("Currently I have " + numFollowers + " followers and I'm currently following " + whoIFollow + "."); //output more information on index.html
