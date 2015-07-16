cc72cf2171d8f148b6d8a2408331d08425b9d522
rcmcnutt <ryanmcnutt@Ryans-MacBook-Pro.local>
2015-07-16 16:58:39 -0400

    Test
    Testing my first alert with values user has selected.

--- McNutt_Ryan_Expressions_Assignment/js/script.js
+++ McNutt_Ryan_Expressions_Assignment/js/script.js
@@ -6,5 +6,9 @@
 
 var creatureEncounter = ["Goblin", "Skeleton", "Troll", "Spider"]; //list of possible creature encounter
 var initiativeRoll = [7, 17, 13, 15]; //initiative rolls for each creature
+var attack = prompt("Pick a number 1 - 4"); //used to show what creature is attacking the player
+
+alert("You've been attacked by a " + creatureEncounter[attack] + " roll 1d20 for initiative!"); //shows the player what is attacking them.
+console.log(attack);
 
 
