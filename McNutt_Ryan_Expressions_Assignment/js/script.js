16fc90ce9dc0dde3af728654e1d69e49ca7463de
rcmcnutt <ryanmcnutt@Ryans-MacBook-Pro.local>
2015-07-16 17:30:18 -0400

    progress
    Updating code to show multiple arrays, casting, and an addition
    modifier. Code to this point works. Need to find a way to show greater
    than or equal to values.

--- McNutt_Ryan_Expressions_Assignment/js/script.js
+++ McNutt_Ryan_Expressions_Assignment/js/script.js
@@ -6,9 +6,15 @@
 
 var creatureEncounter = ["Goblin", "Skeleton", "Troll", "Spider"]; //list of possible creature encounter
 var initiativeRoll = [7, 17, 13, 15]; //initiative rolls for each creature
-var attack = prompt("Pick a number 1 - 4"); //used to show what creature is attacking the player
+var attack = prompt("Pick a number 0 - 3"); //used to show what creature is attacking the player
 
 alert("You've been attacked by a " + creatureEncounter[attack] + " roll 1d20 for initiative!"); //shows the player what is attacking them.
+
+var myRoll = prompt("Please input your die roll."); //place for player to insert their die roll
+var myToHitRoll = Number(myRoll) + 3; //the roll + your to hit modifier
+
+alert("The " + creatureEncounter[attack] + " rolls a " + initiativeRoll[attack] + ", you rolled a " + myToHitRoll + "."); //displays initiative to the player
+
 console.log(attack);
 
 
