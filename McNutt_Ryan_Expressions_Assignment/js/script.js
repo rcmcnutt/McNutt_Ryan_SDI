b067a435bdb4f3476522681e69326bb2d2348bee
rcmcnutt <ryanmcnutt@Ryans-MacBook-Pro.local>
2015-07-16 19:32:38 -0400

    further testing
    Added section for attacking creature and checking outputs to make sure
    it pulls and displays correct information based on prompts.

--- McNutt_Ryan_Expressions_Assignment/js/script.js
+++ McNutt_Ryan_Expressions_Assignment/js/script.js
@@ -6,15 +6,21 @@
 
 var creatureEncounter = ["Goblin", "Skeleton", "Troll", "Spider"]; //list of possible creature encounter
 var initiativeRoll = [7, 17, 13, 15]; //initiative rolls for each creature
+var armorClassRoll = [15, 15, 16, 14]; //armor class for each creature
 var attack = prompt("Pick a number 0 - 3"); //used to show what creature is attacking the player
 
 alert("You've been attacked by a " + creatureEncounter[attack] + " roll 1d20 for initiative!"); //shows the player what is attacking them.
 
 var myRoll = prompt("Please input your die roll."); //place for player to insert their die roll
-var myToHitRoll = Number(myRoll) + 3; //the roll + your to hit modifier
+var myInitiativeRoll = Number(myRoll) + 3; //the roll + your the player's "initiative roll" modifier
 
-alert("The " + creatureEncounter[attack] + " rolls a " + initiativeRoll[attack] + ", you rolled a " + myToHitRoll + "."); //displays initiative to the player
+alert("The " + creatureEncounter[attack] + " rolls a " + initiativeRoll[attack] + ", you rolled a " + myInitiativeRoll + "."); //displays initiative to the player
 
-console.log(attack);
+var attackRoll = prompt("Roll 1d20 to see if you hit the " + creatureEncounter[attack] + "."); //stores the players attack roll
+var myAttack = Number(attackRoll) + 4; //adds the attack roll and the player's "to hit" modifier
+
+alert("Your attack roll is " + myAttack + "." + " The " + creatureEncounter[attack] + "'s armor class is " + armorClassRoll[attack] + "."); //displays the players attack roll vs the creatures armor class
+
+//console.log(attack);
 
 
