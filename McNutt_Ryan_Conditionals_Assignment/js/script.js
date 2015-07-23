6963dd8621cf12399b3072a038ff8df8c2ba8918
rcmcnutt <ryanmcnutt@Ryans-MacBook-Pro.local>
2015-07-23 16:35:53 -0400

    Vars
    Setting up some initial variables and testing some code.

--- McNutt_Ryan_Conditionals_Assignment/js/script.js
+++ McNutt_Ryan_Conditionals_Assignment/js/script.js
@@ -1,7 +1,15 @@
-//Ryan McNutt
-//0004659907
-//Scalable Data Infrastructures
-//C201507 Term
-//Time to buy some comics!
+/*
+Ryan McNutt
+0004659907
+Scalable Data Infrastructures
+C201507 Term
+Time to buy some comics!
+*/
 
 //alert("this is to test my connection between my index.html file and my script.js file.") //testing file setup
+
+var customerName = prompt("Please input your name. \n EX: John Doe"); //asks and stores the name of the customer
+var comicPrice = 2.99; //price of new comics
+var bagPrice = 0.05; //price of 1 single bag
+var boardPrice = 0.05; //price of 1 single board
+var subMember = confirm("Hello " + customerName + "." + "\n Are you a subscriber? \n (OK for Yes, Cancel for No."); //checks if the user is a subscriber
