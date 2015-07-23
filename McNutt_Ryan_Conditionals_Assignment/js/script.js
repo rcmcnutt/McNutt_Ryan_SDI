848d2737a9d4fef38d054b9bf1a7cb0afb246fda
rcmcnutt <ryanmcnutt@Ryans-MacBook-Pro.local>
2015-07-23 13:12:16 -0400

    connection
    Setup file system and tested connection between index.html and
    script.js files.

---
+++ McNutt_Ryan_Conditionals_Assignment/js/script.js
@@ -0,0 +1,7 @@
+//Ryan McNutt
+//0004659907
+//Scalable Data Infrastructures
+//C201507 Term
+//Time to buy some comics!
+
+//alert("this is to test my connection between my index.html file and my script.js file.") //testing file setup
