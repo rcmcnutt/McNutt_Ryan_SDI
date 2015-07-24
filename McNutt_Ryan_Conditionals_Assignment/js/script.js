32a8072ffaa046b2228f2208a53b98c3287bc9c8
rcmcnutt <ryanmcnutt@Ryans-MacBook-Pro.local>
2015-07-23 21:05:35 -0400

    Multiple fixes
    Found multiple issues with my if statements while testing. Removed
    multiple lines to rework code.

--- McNutt_Ryan_Conditionals_Assignment/js/script.js
+++ McNutt_Ryan_Conditionals_Assignment/js/script.js
@@ -12,39 +12,49 @@ Time to buy some comics!
 var comicPrice = 2.99;  //price of new comics
 var bagPrice = 0.05;    //price of 1 single bag
 var boardPrice = 0.05;  //price of 1 single board
-var lowDiscount = 0.1;   //discount for 1-10 subscriptions
-var medDiscount = 0.15;  //discount for 11-30 subscriptions
-var highDiscount = 0.2;  //discount for 31+ subscriptions
+var lowDiscountPrice = 0.1;   //discount for 1-10 subscriptions
+var medDiscountPrice = 0.15;  //discount for 11-30 subscriptions
+var highDiscountPrice = 0.2;  //discount for 31+ subscriptions
 
 var customerName = prompt("May I have your name? \n EX: John"); //asks and stores the name of the customer
 
 if (customerName === ""){
-    prompt("Please input your name. \n EX: John"); //asks and stores the name of the customer
+    customerName = prompt("Please input your name. \n EX: John"); //asks and stores the name of the customer
 }
 
-var subMember = confirm("Hello " + customerName + "." + "\n Are you a Daily Bugle Comics member? \n (OK for Yes, Cancel for No.)"); //checks if the user is a subscriber
+alert("Hello " + customerName + ". Welcome to Daily Bugle Comics!"); //greets the user to the store
 
-var member = (subMember) ? "Thank you for being a member!" : "Becoming a member could save you up to 20% on comics!"; //gives value to subMember response
+subMember = confirm("Are you a Daily Bugle Comics member? \n (OK for Yes, Cancel for No.)"); //checks if the user is a subscriber
 
-alert(member); //displays result of member variable
+var memberOutput = (subMember) ? "Thank you for being a member!" : "Becoming a member could save you up to 20% on comics!"; //gives value to subMember response
 
+alert(memberOutput); //displays result of member variable
 
 var comicNumber = Number(prompt("How many comics will you be purchasing today?")); //asks and stores the amount of comics being purchased
 
-var subNumber = Number(prompt("How many subscriptions do you currently have with Daily Bugle Comics?"));
+if (subMember === true){
 
-if(subNumber <= 10){
+    var subNumber = Number(prompt("How many subscriptions do you currently have with Daily Bugle Comics?")); //asks and stores amount of subscriptions the user has
+
+    if(subNumber <= 10){
     alert("Your current discount is at 10% off new comics!");    //displays the amount of discount based on var subNumber
 
-}else if(subNumber > 10 && subNumber <= 30){
+    }else if(subNumber > 10 && subNumber <= 30){
     alert("Your current discount is at 15% off new comics!");    //displays the amount of discount based on var subNumber
 
-}else if(subNumber > 30){
+    }else if(subNumber > 30){
     alert("Your Current discount is at 20% off new comics!");    //displays the amount of discount based on var subNumber
 
-}else{
-    alert(customerName + " You are missing out on a savings of up to 20%!");
+    }else{
+    alert(customerName + " You are missing out on a savings of up to 20%!"); //alerts user about missing savings because they aren't a member
+    }
+
+}else {
+
+    var subTotal = (comicNumber * comicPrice); //multiples the number of comics by the cost of the comics
+    alert("Your total is " + (subTotal.toFixed(2)) + "."); //testing math on number of comics * the price of comics
 }
+
 
 
 
