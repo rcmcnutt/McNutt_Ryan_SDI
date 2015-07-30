44c1a6e1bbee6eb595d72404bcb7d01b45386e9c
rcmcnutt <ryanmcnutt@Ryans-MacBook-Pro.local>
2015-07-30 02:42:42 -0400

    comments
    Making sure all lines have comments before adding more code. Updated
    and saved.

--- McNutt_Ryan_Functions_Assignment/js/script.js
+++ McNutt_Ryan_Functions_Assignment/js/script.js
@@ -10,16 +10,18 @@
 
 
 //global variables (don't use in functions)
+
 "use strict";
 var userName; //sets a var userName
 var lottoNorm; //sets a var lottoNorm
 var lottoPower; //sets a var lottoPower
 
 
-//lotto functions (Name, PowerBall and Normal FL Lotto)
+//lotto functions (Name, PowerBall, and Normal FL Lotto)
+
 function userCheck(name){ //sets up the function userCheck
 
-    while (name === "" || name === null){ //checks that a name was inserted by user 
+    while (name === "" || name === null){ //checks that a name was inserted by user
         name = prompt("Section blank, Enter your First Name please:"); //if blank, prompts user for name again
 
     }
@@ -28,15 +30,16 @@ function userCheck(name){ //sets up the function userCheck
 
 }
 
-function normLottery(){
+function normLottery(){ //function for generating 6 random lottery numbers between 1-53
 
 }
 
-function powLottery(){
+function powLottery(){ //function for generating 5 random lottery numbers between 1-59 and a Powerball between 1-35
 
 }
 
 //main code (call functions and global variables)
+
 userName = prompt("Welcome to the Florida Lottery,\n Please enter your First Name:"); //collects users name
 userName = userCheck(userName); //calls the function userCheck
 console.log("Thank you " + userName + "."); //displays information in the console for the user
@@ -48,11 +51,11 @@ if (lottoNorm === true) { //sets up an if statement for the type of lottery numb
 
 }else{
 
-    lottoPower = confirm("So you checking the \n Powerball Lottery Numbers? \n (OK for Yes, Cancel for No)");
-    if (lottoPower === true) {
-        console.log ("Thank you for playing the PowerBall Lottery " + userName + ", \n Here are the winning numbers.");
+    lottoPower = confirm("So you checking the \n Powerball Lottery Numbers? \n (OK for Yes, Cancel for No)"); //checks what version of the lotto user would like to display
+    if (lottoPower === true) { //sets up an if statment for the type of lottery numbers the user is checking
+        console.log ("Thank you for playing the PowerBall Lottery " + userName + ", \n Here are the winning numbers."); //outputs a thank you message
     }else{
-        alert("Please refresh the page and try again.");
+        alert("Please refresh the page and try again."); //fail safe alert so user can try again to find numbers
     }
 }
 
