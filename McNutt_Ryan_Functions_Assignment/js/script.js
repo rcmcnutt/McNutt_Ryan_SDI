9e1bc6c8da4fb8b2e52210d58acbcb6ddf36f0da
rcmcnutt <ryanmcnutt@Ryans-MacBook-Pro.local>
2015-07-30 00:10:45 -0400

    removed
    Removed loop counter for the name section being blank. Retested code
    and it works.

--- McNutt_Ryan_Functions_Assignment/js/script.js
+++ McNutt_Ryan_Functions_Assignment/js/script.js
@@ -13,21 +13,16 @@
 var userName = prompt("Welcome to the Florida Lottery,\n Please enter your first name:"); //collects users name
 
 
-//lotto functions (PowerBall and Normal FL Lotto)
+//lotto functions (Name, PowerBall and Normal FL Lotto)
 function userCheck(name){ //sets up the function userCheck
 
-    var leftBlank=1; //keeps count of the times the name section is left blank
     while (name === ""){ //checks that a name was inserted by user
         name = prompt("Insert your first name please:"); //if blank, prompts user for name again
-        leftBlank++;
 
-        if (leftBlank === 5){ //if statement that checks to see how many times name is left blank.
-            console.log ("Please refresh the page and try again."); //message displays if name is left blank 5 times
-            break; //stops loop from running
-        }
     }
 
-    console.log("Thanks for stopping by " + name + "."); //displays information in the console for the user
+    console.log("Thank you " + name + "."); //displays information in the console for the user
+
 }
 
 //main code (call functions and global variables)
